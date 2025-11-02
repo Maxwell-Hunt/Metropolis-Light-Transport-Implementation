7d5817de7da3bf1f9e80523e963ffaef8acd5b4e
Alexander Kaminsky <29873742+kaminskyalexander@users.noreply.github.com>
2025-11-02 13:34:08 -0500

    MIT License

--- src/application.h
+++ src/application.h
@@ -1,3 +1,7 @@
+// Copyright (c) Maxwell Hunt and Alexander Kaminsky 2025. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license
+// information.
+
 #pragma once
 
 #include <chrono>
